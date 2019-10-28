615652b045ba2f2254c35543a9629d3bab5f4b4d
timdl1998 <timdl1998@live.nl>
2019-10-28 20:39:21 +0100

    Added uart data flow in ascii

---
+++ Floaty-wiggle-board/Floaty-wiggle-board.sdk/FWB_software/Debug/src/helloworld.d
@@ -0,0 +1,39 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../FWB_software_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../FWB_software_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../FWB_software_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../FWB_software_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../FWB_software_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../FWB_software_bsp/ps7_cortexa9_0/include/sleep.h \
+ ../../FWB_software_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../FWB_software_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../FWB_software_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../FWB_software_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../FWB_software_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../FWB_software_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../FWB_software_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../FWB_software_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../FWB_software_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../FWB_software_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../FWB_software_bsp/ps7_cortexa9_0/include/sleep.h:
+
+../../FWB_software_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../FWB_software_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../FWB_software_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../FWB_software_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../FWB_software_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
---
+++ Floaty-wiggle-board/Floaty-wiggle-board.sdk/FWB_software/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../FWB_software_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../FWB_software_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../FWB_software_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../FWB_software_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../FWB_software_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../FWB_software_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../FWB_software_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../FWB_software_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
