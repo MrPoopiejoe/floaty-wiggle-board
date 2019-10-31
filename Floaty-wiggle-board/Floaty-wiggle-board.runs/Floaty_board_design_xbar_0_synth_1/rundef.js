7f4e402ff73786225d73a2b9af2785b4486de416
Henry <perfectlyfrancis@gmail.com>
2019-10-31 11:37:29 +0100

    this is broken

--- Floaty-wiggle-board/Floaty-wiggle-board.runs/Floaty_board_design_xbar_0_synth_1/rundef.js
+++ Floaty-wiggle-board/Floaty-wiggle-board.runs/Floaty_board_design_xbar_0_synth_1/rundef.js
@@ -22,8 +22,8 @@ var ISEJScriptLib = RDScrDir + "/ISEWrap.js";
 eval( EAInclude(ISEJScriptLib) );
 
 
-// ISEStep( "vivado",
-//          "-log Floaty_board_design_xbar_0.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source Floaty_board_design_xbar_0.tcl" );
+ISEStep( "vivado",
+         "-log Floaty_board_design_xbar_0.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source Floaty_board_design_xbar_0.tcl" );
 
 
 
